93af0ab198c69061a505a39772a85d075d360dd9
Matt Stevens <matt@mempkin.co.uk>
2025-05-27 00:40:22 +0100

    Fix Firefox Developer Edition detection in scripts

--- noctrlq.sh
+++ noctrlq.sh
@@ -2,6 +2,6 @@
 
 W="$(xdotool getactivewindow)"
 S1="$(xprop -id "$W" | awk -F '"' '/WM_CLASS/{print tolower($4)}')"
-if [ "$S1" != "firefox" ] && [ "$S1" != "firefox developer edition" ]; then
+if [ "$S1" != "firefox" ] && [ "$S1" != "firefox-developer-edition" ]; then
 	xdotool key --clearmodifiers --window "$W" "ctrl+q"
 fi
